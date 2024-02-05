415d883dbbb407a61eb99b8210ebf666f54978d8
santicano <canosantiago404@gmail.com>
2024-02-04 21:42:16 -0300

    añadido update list action

---
+++ actions/update-list/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const UpdateList = z.object({
+  title: z
+    .string({
+      required_error: "Nombre es requerido",
+      invalid_type_error: "Nombre es requerido",
+    })
+    .min(3, {
+      message: "Nombre es muy corto",
+    }),
+  id: z.string(),
+  boardId: z.string(),
+});
