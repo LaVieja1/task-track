714f7c0695123945d519c5e6f2c1374c25f41b2a
santicano <canosantiago404@gmail.com>
2024-02-05 20:41:26 -0300

    añadido queryProvider a layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -3,13 +3,16 @@ import { esES } from "@clerk/localizations";
 import { Toaster } from "sonner";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider localization={esES}>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
