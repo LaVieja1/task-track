013ef940810d9d5346da6f8ec3da4565ecdc83ed
santicano <canosantiago404@gmail.com>
2024-02-05 20:35:51 -0300

    añadido modalProvider a layout y useCardModal a card-item

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,10 +2,13 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { esES } from "@clerk/localizations";
 import { Toaster } from "sonner";
 
+import { ModalProvider } from "@/components/providers/modal-provider";
+
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider localization={esES}>
       <Toaster />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   );
