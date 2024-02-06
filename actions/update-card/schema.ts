7164f16a6d0d259900c36c4d8cf01b1d2ccd10d2
santicano <canosantiago404@gmail.com>
2024-02-06 11:26:12 -0300

    añadido titulo opcional

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -12,13 +12,15 @@ export const UpdateCard = z.object({
         message: "Descripción es muy corta",
       })
   ),
-  title: z
-    .string({
-      required_error: "Nombre es requerido",
-      invalid_type_error: "Nombre es requerido",
-    })
-    .min(3, {
-      message: "Nombre es muy corto",
-    }),
+  title: z.optional(
+    z
+      .string({
+        required_error: "Nombre es requerido",
+        invalid_type_error: "Nombre es requerido",
+      })
+      .min(3, {
+        message: "Nombre es muy corto",
+      })
+  ),
   id: z.string(),
 });
