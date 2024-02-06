c4a960bfbba380f747b355323180bba44da4734c
santicano <canosantiago404@gmail.com>
2024-02-06 17:15:48 -0300

    añadido isPro a delete-board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -9,6 +9,7 @@ import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { decreaseAvailableCount } from "@/lib/org-limit";
 import { createAuditLog } from "@/lib/create-audit-log";
+import { checkSubscription } from "@/lib/subscription";
 
 import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
@@ -22,6 +23,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
+  const isPro = await checkSubscription();
+
   const { id } = data;
   let board;
 
@@ -33,7 +36,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    await decreaseAvailableCount();
+    if (!isPro) {
+      await decreaseAvailableCount;
+    }
 
     await createAuditLog({
       entityTitle: board.title,
