e6aac3b119dc1c3599770fd2ebf00d4e03ba517b
santicano <canosantiago404@gmail.com>
2024-02-05 21:47:40 -0300

    añadido accion update card

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const UpdateCard = z.object({
+  boardId: z.string(),
+  description: z.optional(
+    z
+      .string({
+        required_error: "Descripción es requerida",
+        invalid_type_error: "Descripción es requerida",
+      })
+      .min(3, {
+        message: "Descripción es muy corta",
+      })
+  ),
+  title: z
+    .string({
+      required_error: "Nombre es requerido",
+      invalid_type_error: "Nombre es requerido",
+    })
+    .min(3, {
+      message: "Nombre es muy corto",
+    }),
+  id: z.string(),
+});
