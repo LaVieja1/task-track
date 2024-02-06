76e7e9bec392a57ab7efec64e49dc5b816324875
santicano <canosantiago404@gmail.com>
2024-02-06 15:20:13 -0300

    agregado cuando se borra un tablero, se suma a los restantes

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -3,14 +3,15 @@
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
+import { decreaseAvailableCount } from "@/lib/org-limit";
+import { createAuditLog } from "@/lib/create-audit-log";
 
 import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
-import { createAuditLog } from "@/lib/create-audit-log";
-import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -31,6 +32,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+
+    await decreaseAvailableCount();
+
     await createAuditLog({
       entityTitle: board.title,
       entityId: board.id,
